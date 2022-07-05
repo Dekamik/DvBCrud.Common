82f75518c716f746ef79eff9d70f0181d16b1fa7
Dennis von Bargen <dennis.von-bargen@viati.se>
2022-07-05 12:17:36 +0200

    Added CrudControllerBase

---
+++ DvBCrud.Common.Api/Controllers/CrudControllerBase.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DvBCrud.Common.Api.Controllers;
+
+public abstract class CrudControllerBase<TModel> : ControllerBase
+{
+    protected ObjectResult Forbidden() => StatusCode(403, $"Action forbidden on {nameof(TModel)}");
+}
