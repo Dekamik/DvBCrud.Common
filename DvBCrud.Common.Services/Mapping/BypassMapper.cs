e0ad48869dfafe4e127de95ff62810c8b01641f2
Dennis von Bargen <dennis.von-bargen@viati.se>
2023-01-30 23:52:18 +0100

    Renamed converters to mappers

---
+++ DvBCrud.Common.Services/Mapping/BypassMapper.cs
@@ -0,0 +1,10 @@
+﻿namespace DvBCrud.Common.Services.Mapping;
+
+/// <inheritdoc cref="IBypassMapper{TDataModel}"/>
+public abstract class BypassMapper<TDataModel> : IBypassMapper<TDataModel>
+    where TDataModel : class
+{
+    public TDataModel ToModel(TDataModel dataModel) => dataModel;
+
+    public TDataModel ToEntity(TDataModel apiModel) => apiModel;
+}
---
+++ DvBCrud.Common.Services/Mapping/IBypassMapper.cs
@@ -0,0 +1,13 @@
+﻿namespace DvBCrud.Common.Services.Mapping;
+
+/// <summary>
+/// Simple implementation to override the conversion logic of <typeparamref name="TEntity"/>.
+/// Use this if you intend to expose your data model directly.
+///
+/// When you decide to implement your own converter, inherit from <see cref="IMapper{TDataModel,TApiModel}"/> instead.
+/// </summary>
+/// <typeparam name="TEntity">Entity/data model type</typeparam>
+public interface IBypassMapper<TEntity> : IMapper<TEntity, TEntity>
+    where TEntity : class
+{
+}
